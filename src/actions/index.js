d7aa372aab2100a0e12486f48bd27f3487d3cc03
Pierre Besson <pierre.besson7@gmail.com>
2016-02-08 13:49:40 +0100

    [index] Add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,24 @@
+import fetch from 'isomorphic-fetch';
 console.log('actions');
+
+
+export const REQUEST_ENTITY = 'REQUEST_ENTITY';
+export const RECEIVE_ENTITY = 'RECEIVE_ENTITY';
+
+function requestEntity({id}){
+  return {type: REQUEST_ENTITY, payload: {id}};
+}
+
+function receiveEntity({id}){
+  return {type: RECEIVE_ENTITY, payload: {id}};
+}
+
+
+export function fetchEntity({id}){
+  return function middleWareFetchCall(dispatch){
+    dispatch(requestEntity({id}));
+    return fetch(`http://localhost:9999/x/entity/${id}`)
+            .then(response => response.json())
+            .then(json => dispatch(receiveEntity(json)));
+  }
+}
