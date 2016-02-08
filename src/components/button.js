6fa34f16e1da24654f91fb843d9151c61f17eef6
Pierre Besson <pierre.besson7@gmail.com>
2016-02-08 13:07:34 +0100

    [button] Add the button

---
+++ src/components/button.js
@@ -0,0 +1,21 @@
+import React, {PropTypes} from 'react';
+
+function Button({onClick, children, options}){
+  const optionsClassName = options.map(opt => `mdl-button--${opt}`).join(' ');
+  return (
+    <button className={`mdl-button mdl-js-button ${optionsClassName}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+
+Button.displayName = 'Button';
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string)
+};
+Button.defaultProps = {
+  options: []
+}
+export default Button;
