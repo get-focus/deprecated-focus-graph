ad07c81ace686e87ea41df174d8716975f635c70
Pierre Besson <pierre.besson7@gmail.com>
2016-02-10 15:56:23 +0100

    [action] err handling

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ console.log('actions');
 
 export const REQUEST_ENTITY = 'REQUEST_ENTITY';
 export const RECEIVE_ENTITY = 'RECEIVE_ENTITY';
+export const ERROR_ENTITY = 'ERROR_ENTITY';
 
 function requestEntity({id}){
   return {type: REQUEST_ENTITY, payload: {id}};
@@ -13,12 +14,16 @@ function receiveEntity(jsonEntity){
   return {type: RECEIVE_ENTITY, payload: jsonEntity};
 }
 
+function errorEntity(error){
+  return {type: ERROR_ENTITY, payload: error};
+}
 
 export function fetchEntity({id}){
   return function middleWareFetchCall(dispatch){
     dispatch(requestEntity({id}));
     return fetch(`http://localhost:9999/x/entity/${id}`)
             .then(response => response.json())
-            .then(json => dispatch(receiveEntity(json)));
+            .then(json => dispatch(receiveEntity(json)))
+            .catch(err => dispatch(errorEntity(err)));
   }
 }
