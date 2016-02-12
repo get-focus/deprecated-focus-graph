799c5c1762e048f4f8cc5158894aab777dab9686
Pierre Besson <pierre.besson7@gmail.com>
2016-02-12 16:18:59 +0100

    [service]

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,6 +1,6 @@
-import {loadActionBuilder} from './entity-actions-builder';
+import {actionBuilder} from './entity-actions-builder';
+import {loadUserSvc} from '../services/user-service';
 
-const loadAction = loadActionBuilder('user', 'load');
-
+const loadAction = actionBuilder({name:'user', type:'load', service:loadUserSvc});
 export const loadUserTypes = loadAction.types;
 export const loadUserAction = loadAction.action;
