74b03931703770f299f6e80937b2cad93856ec54
Pierre Besson <pierre.besson7@gmail.com>
2016-02-14 11:20:17 +0100

    [reducer builder] imporve doc

--- src/reducers/reducer-builder.js
+++ src/reducers/reducer-builder.js
@@ -1,5 +1,7 @@
 // Build a reducer in order to ease the creation on these in 80% of the cases
+// ## doc
 // To build a reducer you need to provide an object which is
+// ```javascript
 // {
 //  types : {
 //    load: { request, receive, error} which are the types for the load action
@@ -7,6 +9,18 @@
 //  },
 //  defaultState: the default state of your reducer
 // }
+// ```
+//
+// ## example
+// ```javascript
+// const DEFAULT_USER_STATE = {firstName: 'Test'};
+// const userReducer = buildReducer(
+//  {
+//    load: {request: REQUEST_LOAD_USER, receive: RECEIVE_LOAD_USER},
+//    save: {request: REQUEST_SAVE_USER, receive: RECEIVE_SAVE_USER}
+//  }
+// )
+// ```
 export const buildReducer({types, defaultState})=>((state, {type, payload})=>{
   const {load, save} = types;
   const {data} = state;
