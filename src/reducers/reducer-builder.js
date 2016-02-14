9cb13b4302a43300d55adc8a288fdebfbd3f399d
Pierre Besson <pierre.besson7@gmail.com>
2016-02-14 15:57:56 +0100

    [reducer builder] Add doc and fix issues.

--- src/reducers/reducer-builder.js
+++ src/reducers/reducer-builder.js
@@ -14,23 +14,39 @@
 // ## example
 // ```javascript
 // const DEFAULT_USER_STATE = {firstName: 'Test'};
-// const userReducer = buildReducer(
-//  {
+// const userReducer = buildReducer({types: {
 //    load: {request: REQUEST_LOAD_USER, receive: RECEIVE_LOAD_USER},
 //    save: {request: REQUEST_SAVE_USER, receive: RECEIVE_SAVE_USER}
-//  }
-// )
+//  },
+//  defaultState: DEFAULT_USER_STATE
+// })
 // ```
-export const buildReducer({types, defaultState})=>((state, {type, payload})=>{
+// will produce
+// ```javascript
+// function userReducer(state = DEFAULT_STATE, {type, payload}){
+// const {data} = state;
+//  switch (type) {
+//   case REQUEST_LOAD_USER:
+//       return {data, isLoading: true};
+//   case RECEIVE_LOAD_USER:
+//       return {data: payload, isLoading: false};
+//   default:
+//       return state
+//  }
+// }
+// ```
+export const reducerBuilder = ({types, defaultState})=>((state = defaultState, {type, payload})=>{
+  //todo: add some validation and check here
   const {load, save} = types;
   const {data} = state;
-  switch(tyoe){
+  switch(type){
     case load.request:
     case save.request:
      return {data, isLoading: true};
    case load.receive:
    case save.receive:
-      return {payload, isLoading: false};
+      return {data: payload, isLoading: false};
+  // todo: error case
    default:
      return state;
   }
