38ed8263577ca6017836b224b7adaf1cbb687f3f
Pierre Besson <pierre.besson7@gmail.com>
2016-02-14 11:14:12 +0100

    [reducer builder] Add doc

--- src/reducers/reducer-builder.js
+++ src/reducers/reducer-builder.js
@@ -1,5 +1,12 @@
-import {loadUserTypes} from '../actions/user-actions';
-
+// Build a reducer in order to ease the creation on these in 80% of the cases
+// To build a reducer you need to provide an object which is
+// {
+//  types : {
+//    load: { request, receive, error} which are the types for the load action
+//    save: { request, receive, error} which are the types for the save action
+//  },
+//  defaultState: the default state of your reducer
+// }
 export const buildReducer({types, defaultState})=>((state, {type, payload})=>{
   const {load, save} = types;
   const {data} = state;
