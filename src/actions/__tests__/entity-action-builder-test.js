b7432aede6755dba8708593188651194145b66c3
Pierre Besson <pierre.besson7@gmail.com>
2016-02-15 10:08:03 +0100

    [action builder] focus on message instead of error type.

--- src/actions/__tests__/entity-action-builder-test.js
+++ src/actions/__tests__/entity-action-builder-test.js
@@ -11,14 +11,25 @@ describe('The actionBuilder', () => {
         );
         it('should throw a type error when called without all parameters', () => {
             expect(()=>{ actionBuilder()}).to.throw(TypeError,"Cannot read property 'name' of undefined");
-//            name, type, service
+            //name, type, service
         });
-        it('should throw a type error when called without the good name parameter', () => {
+        it('should throw an error when called without a string name parameter', () => {
             const NAME_MESSAGE = 'ACTION_BUILDER: the name parameter should be a string.';
-            expect(()=>{ actionBuilder({name: undefined})}).to.throw(Error, NAME_MESSAGE);
-            expect(()=>{ actionBuilder({name: 1})}).to.throw(Error, NAME_MESSAGE);
-            expect(()=>{ actionBuilder({name: {}})}).to.throw(Error, NAME_MESSAGE);
-            expect(()=>{ actionBuilder({name: ()=>{}})}).to.throw(Error, NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: undefined})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 1})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: {}})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: ()=>{}})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test'})}).to.not.throw(NAME_MESSAGE);
+        });
+        it('should throw an error when called without a string type parameter : load,save,delete', () => {
+            const NAME_MESSAGE = 'ACTION_BUILDER: the type parameter should be a string and the value one of these: load,save,delete.';
+            expect(()=>{ actionBuilder({name: 'test'})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test', type: undefined})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test', type: 1})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test', type: {}})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test', type: ()=>{}})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test', type: ''})}).to.throw(NAME_MESSAGE);
+            expect(()=>{ actionBuilder({name: 'test', type: 'nimp'})}).to.throw(NAME_MESSAGE);
         });
     })
 });
