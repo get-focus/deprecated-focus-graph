3a1b53df624cd3fbadc643c8777f36b8f7141b55
bernardstanislas <bernardstanislas@gmail.com>
2016-03-16 18:29:33 +0100

    Fix middleware, add tests

---
+++ src/middlewares/__tests__/form-middleware-test.js
@@ -0,0 +1,142 @@
+import formMiddleware from '../form';
+import {CREATE_FORM, SYNC_FORM_ENTITY} from '../../actions/form';
+
+describe('The form middleware', () => {
+    const getStateSpy = sinon.spy();
+    const nextSpy = sinon.spy();
+    const dispatchSpy = sinon.spy();
+    const store = {
+        getState: () => {
+            getStateSpy();
+            return {
+                dataset: {
+                    user: {
+                        data: {
+                            firstName: 'Joe',
+                            lastName: 'Lopez'
+                        },
+                        loading: true
+                    }
+                },
+                forms: [{
+                    key: 'formKey',
+                    entityPathArray: ['user'],
+                    fields: [{
+                        name: 'firstName',
+                        dataSetValue: 'Joe',
+                        entityPath: 'user',
+                        loading: false
+                    }]
+                }]
+            }
+        },
+        dispatch: dispatchSpy
+    };
+    beforeEach(() => {
+        getStateSpy.reset();
+        nextSpy.reset();
+        dispatchSpy.reset();
+    })
+    describe('when a random action is passed', () => {
+        const randomAction = {
+            type: 'LOL'
+        };
+        it('should just pass the action to next', () => {
+            formMiddleware(null)(nextSpy)(randomAction);
+            expect(nextSpy).to.have.been.callCount(1);
+            expect(nextSpy).to.have.been.calledWith(randomAction);
+        })
+    });
+    describe('when a CREATE_FORM action is passed', () => {
+        const creationAction = {
+            type: CREATE_FORM,
+            key: 'formKey',
+            entityPathArray: ['user']
+        }
+        it('should call the store.getState once', () => {
+            formMiddleware(store)(nextSpy)(creationAction);
+            expect(getStateSpy).to.have.callCount(1);
+        });
+        it('should call the next once with the populated data', () => {
+            formMiddleware(store)(nextSpy)(creationAction);
+            expect(nextSpy).to.have.been.callCount(1);
+            expect(nextSpy).to.have.been.calledWith({
+                ...creationAction,
+                fields: [
+                    {
+                        dataSetValue: 'Joe',
+                        entityPath: 'user',
+                        name: 'firstName',
+                        loading: true
+                    },
+                    {
+                        dataSetValue: 'Lopez',
+                        entityPath: 'user',
+                        name: 'lastName',
+                        loading: true
+                    }
+                ]
+            });
+        });
+    });
+    describe('when an action that should sync the form is passed', () => {
+        const alteredStore = {
+            getState: () => {
+                getStateSpy();
+                return {
+                    dataset: {
+                        user: {
+                            data: {
+                                firstName: 'David',
+                                lastName: 'Lopez'
+                            },
+                            loading: true
+                        }
+                    },
+                    forms: [{
+                        key: 'formKey',
+                        entityPathArray: ['user'],
+                        fields: [{
+                            name: 'firstName',
+                            dataSetValue: 'Joe',
+                            entityPath: 'user',
+                            loading: false
+                        }]
+                    }]
+                }
+            },
+            dispatch: dispatchSpy
+        };
+        const myLoadAction = {
+            type: 'A_TYPE_THAT_THE_FORM_SHOULD_LOOK_AFTER',
+            syncForm: true,
+            entityPath: 'user'
+        }
+        it('should pass the provided action to next', () => {
+            formMiddleware(alteredStore)(nextSpy)(myLoadAction);
+            expect(nextSpy).to.have.callCount(1);
+        })
+        it('should dispatch several SYNC_FORM_ENTITY actions', () => {
+            formMiddleware(alteredStore)(nextSpy)(myLoadAction);
+            expect(dispatchSpy).to.have.callCount(1);
+            expect(dispatchSpy).to.have.been.calledWith({
+                type: SYNC_FORM_ENTITY,
+                entityPath: 'user',
+                fields: [
+                    {
+                        dataSetValue: 'David',
+                        entityPath: 'user',
+                        name: 'firstName',
+                        loading: true
+                    },
+                    {
+                        dataSetValue: 'Lopez',
+                        entityPath: 'user',
+                        name: 'lastName',
+                        loading: true
+                    }
+                ]
+            });
+        })
+    });
+});
--- src/middlewares/form.js
+++ src/middlewares/form.js
@@ -1,15 +1,36 @@
 import {CREATE_FORM} from '../actions/form';
+import {syncFormEntity} from '../actions/form';
 import get from 'lodash/get';
+import map from 'lodash/map';
 
 const formMiddleware = store => next => action => {
     if (action.type === CREATE_FORM) {
         const {dataset} = store.getState();
         const {entityPathArray, key: formKey} = action;
-        const fields = entityPathArray.reduce((acc, entityPath) => ({...acc, [entityPath]: get(dataset, entityPath)}), {});
+        const fields = entityPathArray.reduce((acc, entityPath) => ([
+            ...acc,
+            ...map(get(dataset, `${entityPath}.data`), (fieldValue, fieldName) => ({
+                name: fieldName,
+                entityPath,
+                dataSetValue: fieldValue,
+                loading: get(dataset, `${entityPath}.loading`)
+            }))
+        ]), []);
         return next({...action, fields});
-    } else if (action.impactForm) {
-        const {forms} = store.getState();
-        return next(action);
+    } else if (action.syncForm) {
+        // The action requires the forms to sync themselves with the dataset, let's do it
+        // Grab the new state, to have the updates on the dataset
+        const newState = next(action);
+        const {dataset} = store.getState();
+        const entityPath = action.entityPath;
+        const fields = map(get(dataset, `${entityPath}.data`), (fieldValue, fieldName) => ({
+            name: fieldName,
+            entityPath,
+            dataSetValue: fieldValue,
+            loading: get(dataset, `${entityPath}.loading`)
+        }));
+        store.dispatch(syncFormEntity(entityPath, fields));
+        return newState;
     } else {
         return next(action);
     }
