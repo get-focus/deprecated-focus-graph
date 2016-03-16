7202a5fe5698f0440446f8f812bd1db1a2137e42
bernardstanislas <bernardstanislas@gmail.com>
2016-03-16 18:47:19 +0100

    Add comments

--- src/middlewares/form.js
+++ src/middlewares/form.js
@@ -21,15 +21,19 @@ const formMiddleware = store => next => action => {
         // The action requires the forms to sync themselves with the dataset, let's do it
         // Grab the new state, to have the updates on the dataset
         const newState = next(action);
+        // Get the updated dataset
         const {dataset} = store.getState();
         const entityPath = action.entityPath;
+        // Read the fields in the dataset at the entityPath location, and build a minimum field object that will be merged with the form fields
         const fields = map(get(dataset, `${entityPath}.data`), (fieldValue, fieldName) => ({
             name: fieldName,
             entityPath,
             dataSetValue: fieldValue,
             loading: get(dataset, `${entityPath}.loading`)
         }));
+        // Dispatch the SYNC_FORM_ENTITY action
         store.dispatch(syncFormEntity(entityPath, fields));
+        // Continue with the rest of the redux flow
         return newState;
     } else {
         return next(action);
--- src/reducers/form.js
+++ src/reducers/form.js
@@ -27,14 +27,20 @@ const form = (state = [], action) => {
         case SYNC_FORM_ENTITY:
             // Iterate over all forms, and synchronise fields with the dataset
             return state.map(form => ({
+                // Get the rest of the form
                 ...form,
+                // If the entityPath is in the form's entityPathArray, then mutate its fields
                 ...(form.entityPathArray.indexOf(action.entityPath) !== -1 ? {
                     fields: [
                         ...form.fields.map(formField => ({
                             ...formField,
+                            // Look into the action's fields to update, if it concerns the current field then merge it
                             ...find(action.fields, {entityPath: action.entityPath, name: formField.name})
                         })),
+                        // Get the outter intersection of the action fields and the form fields, meaning the
+                        // fields to update that are not in the form, or the form fields that should not be updated
                         ...xorWith(form.fields, action.fields, (a, b) => a.name === b.name && a.entityPath === b.entityPath)
+                        // then filter it to remove the existing form fields, and create the missing fields
                         .filter(({dirty}) => isUndefined(dirty))
                         .map(initializeField)
                     ]
