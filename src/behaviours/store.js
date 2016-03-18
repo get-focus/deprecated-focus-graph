10ba5ed903daadc5463622f6a4de356b165c980a
bernardstanislas <bernardstanislas@gmail.com>
2016-03-18 17:48:29 +0100

    Add TODO on store behaviour

--- src/behaviours/store.js
+++ src/behaviours/store.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {connect as connectToReduxStore } from 'react-redux';
 import get from 'lodash/get';
 
+// TODO : review this one
 const defaultMapStateToPropsBuilder = entityPathArray => ({dataset}) => entityPathArray.reduce((acc, entityPath) => ({...acc, fields: {...acc.fields, [entityPath]: get(dataset, entityPath)}}), {fields: {}});
-const defaultMapDispatchToProps = dispatch => ({dispatch});
 
-export const connect = (entityPathArray, {mapStateToProps, mapDispatchToProps = defaultMapDispatchToProps}) => connectToReduxStore(mapStateToProps || defaultMapStateToPropsBuilder(entityPathArray), mapDispatchToProps);
+export const connect = (entityPathArray, {mapStateToProps, mapDispatchToProps}) => connectToReduxStore(mapStateToProps || defaultMapStateToPropsBuilder(entityPathArray), mapDispatchToProps);
