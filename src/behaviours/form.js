5d7fea876c5676efa44a6ff16ee1a58f79822a20
bernardstanislas <bernardstanislas@gmail.com>
2016-04-11 12:08:07 +0200

    Add action handling

--- src/behaviours/form.js
+++ src/behaviours/form.js
@@ -2,36 +2,52 @@ import React, {Component, PropTypes} from 'react';
 import {connect as connectToStore} from './store';
 import {createForm, destroyForm, inputChange} from '../actions/form';
 import find from 'lodash/find';
+import compose from 'lodash/flowRight';
 
-const mapStateToPropsBuilder = key => ({forms}) => {
-    const formCandidate = find(forms, {key});
-    return {fields: formCandidate ? formCandidate.fields : []};
+const internalMapStateToProps = (state, key) => {
+    const formCandidate = find(state.forms, {key});
+    const resultingProps = {};
+    if (formCandidate) {
+        resultingProps.fields = formCandidate.fields;
+        resultingProps.getUserInput = () => formCandidate.fields.reduce((entity, field) => ({...entity, [field.name]: field.inputValue}), {});
+    } else {
+        resultingProps.fields = [];
+        resultingProps.getUserInput = () => ({});
+    }
+    return resultingProps;
 };
 
-export const connect = (formKey, entityPathArray, storeConnectorOptions) => ComponentToConnect => {
+const internalMapDispatchToProps = (dispatch, loadAction, saveAction) => {
+    const resultingActions = {};
+    if (loadAction) resultingActions.load = compose(dispatch, loadAction);
+    if (saveAction) resultingActions.save = compose(dispatch, saveAction);
+    return resultingActions;
+};
+
+const getExtendedComponent = (ComponentToConnect,formOptions) => {
     class FormComponent extends Component {
         componentWillMount() {
             const {store: {dispatch}} = this.context;
-            dispatch(createForm(formKey, entityPathArray));
+            dispatch(createForm(formOptions.name, formOptions.entityPathArray));
         }
 
         componentWillUnmount() {
             const {store: {dispatch}} = this.context;
-            dispatch(destroyForm(formKey));
+            dispatch(destroyForm(formOptions.name));
         }
 
         _onInputChange(name, entityPath, value) {
             const {store: {dispatch}} = this.context;
-            dispatch(inputChange(formKey, name, entityPath, value));
+            dispatch(inputChange(formOptions.name, name, entityPath, value));
         }
 
         _toggleEdit(edit) {
             const {store: {dispatch}} = this.context;
-            dispatch(toggleFormEdit(formKey, edit));
+            dispatch(toggleFormEdit(formOptions.name, edit));
         }
 
         render() {
-            return <ComponentToConnect {...this.props} onInputChange={::this._onInputChange} entityPathArray={entityPathArray}/>;
+            return <ComponentToConnect {...this.props} onInputChange={::this._onInputChange} entityPathArray={formOptions.entityPathArray}/>;
         }
     }
     FormComponent.contextTypes = {
@@ -41,5 +57,33 @@ export const connect = (formKey, entityPathArray, storeConnectorOptions) => Comp
             getState: PropTypes.func.isRequired
         })
     };
-    return connectToStore(entityPathArray, {mapStateToProps: mapStateToPropsBuilder(formKey), ...storeConnectorOptions})(FormComponent);
+    return FormComponent;
+};
+
+export const connect = (formOptions) => ComponentToConnect => {
+    const {
+        name: formKey,
+        entityPathArray,
+        mapStateToProps: userDefinedMapStateToProps = () => ({}),
+        mapDispatchToProps: userDefinedMapDispatchToProps = () => ({}),
+        loadAction,
+        saveAction
+    } = formOptions;
+
+    // Extend the component
+    const extendedComponent = getExtendedComponent(ComponentToConnect, formOptions);
+
+    const mapStateToProps = state => ({
+        ...internalMapStateToProps(state, formKey),
+        ...userDefinedMapStateToProps(state)
+    });
+    const mapDispatchToProps = dispatch => ({
+        ...internalMapDispatchToProps(dispatch, loadAction, saveAction),
+        ...userDefinedMapDispatchToProps(dispatch)
+    });
+
+    return connectToStore(entityPathArray, {
+        mapStateToProps,
+        mapDispatchToProps}
+    )(extendedComponent);
 }
--- src/example/components/user-dumb.js
+++ src/example/components/user-dumb.js
@@ -11,32 +11,32 @@ import Button from './button';
 import Code from './code';
 import compose from 'lodash/flowRight';
 
-function UserDumbComponent({fields, onChange, onSubmit, fieldFor, createForm, loadEntity, saveEntity, id, ...otherProps}) {
-    //console.log('dubm props', onChange, onSubmit)
-    const save = () => saveEntity(fields.reduce((user, field) => ({...user, [field.name]: field.inputValue}), {}));
+function UserDumbComponent({fields, fieldFor, load, save, id, getUserInput, ...otherProps}) {
     return (
         <div>
             {/* Fields auto rendering to test direct rendering without helpers*/}
-            <Button onClick={() => {loadEntity(id)}}>Load entity from server</Button>
+            <Button onClick={() => {load({id})}}>Load entity from server</Button>
             {fieldFor('uuid', {onChange: () => {console.log(fields)}})}
             {fieldFor('firstName')}
             {fieldFor('lastName')}
-            <Button onClick={save}>{'Save'}</Button>
+            <Button onClick={() => save(getUserInput())}>{'Save'}</Button>
         </div>
     );
 }
 
 UserDumbComponent.displayName = UserDumbComponent;
 
+const formConfig = {
+    name: 'userForm',
+    entityPathArray: ['user'],
+    loadAction: loadUserAction,
+    saveAction: saveUserAction
+};
+
 //Connect the component to all its behaviours (respect the order for store, store -> props, helper)
 const ConnectedUserDumbComponent = compose(
     connectToDefinitions('user'),
-    formConnect('userForm', ['user'], {
-        mapDispatchToProps: dispatch => ({
-            loadEntity: id => dispatch(loadUserAction({id})),
-            saveEntity: user => dispatch(saveUserAction(user))
-        })
-    }),
+    formConnect(formConfig),
     connectToFieldHelpers()
 )(UserDumbComponent);
 
