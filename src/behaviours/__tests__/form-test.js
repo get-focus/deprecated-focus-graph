8e0bc1b5318a1710645d6c85a748ea26c3206475
bernardstanislas <bernardstanislas@gmail.com>
2016-04-11 17:21:55 +0200

    Add tests for the form connect

---
+++ src/behaviours/__tests__/form-test.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import {connect} from '../form';
+import {Provider as StoreProvider} from 'react-redux';
+import store from '../../example/store';
+const {renderIntoDocument} = TestUtils;
+import find from 'lodash/find';
+
+describe('The form connect', () => {
+    let capturedProps;
+    const MyComponent = (props) => {
+        capturedProps = props;
+        return (<div>my component</div>);
+    }
+
+    describe('when used with no option', () => {
+        const formOptions = {};
+        it('should throw an error on the formKey option', () => {
+            expect(() => connect(formOptions)(MyComponent)).to.throw('You must provide a "formKey" option as a string to the form connect.');
+        });
+    });
+
+    describe('when used with only the name option', () => {
+        const formOptions = {
+            formKey: 'testForm'
+        };
+        it('should throw an error on the entityPathArray option', () => {
+            expect(() => connect(formOptions)(MyComponent)).to.throw('You must provide a "entityPathArray" option as an array to the form connect.');
+        });
+    });
+
+    describe('when used with a name and an entityPathArray', () => {
+        const formOptions = {
+            formKey: 'testForm',
+            entityPathArray: ['user']
+        };
+        const ConnectedTestComponent = connect(formOptions)(MyComponent);
+        const TestWrapperComponent = () => (
+            <StoreProvider store={store}>
+                <ConnectedTestComponent/>
+            </StoreProvider>
+        );
+
+        let renderedTestComponent;
+        before(() => {
+            renderedTestComponent = renderIntoDocument(<TestWrapperComponent/>);
+        });
+
+        it('should not throw any error', () => {
+            expect(() => connect(formOptions)(MyComponent)).to.not.throw();
+        });
+        it('should have the form state in the props', () => {
+            expect(capturedProps.formKey).to.equal('testForm');
+            expect(capturedProps.loading).to.be.false;
+            expect(capturedProps.editing).to.be.false;
+            expect(capturedProps.saving).to.be.false;
+            expect(capturedProps.entityPathArray).to.deep.equal(['user']);
+        });
+    });
+    describe('when onInputChange is called', () => {
+        const dispatchSpy = sinon.spy(store, 'dispatch');
+        const formOptions = {
+            formKey: 'testForm',
+            entityPathArray: ['user']
+        };
+        const ConnectedTestComponent = connect(formOptions)(MyComponent);
+        const TestWrapperComponent = () => (
+            <StoreProvider store={store}>
+                <ConnectedTestComponent/>
+            </StoreProvider>
+        );
+        let renderedTestComponent;
+        before(() => {
+            dispatchSpy.reset();
+            renderedTestComponent = renderIntoDocument(<TestWrapperComponent/>);
+            const {onInputChange} = capturedProps;
+            onInputChange('uuid', 'user', 'new value');
+        });
+        it('should trigger an INPUT_CHANGE action', () => {
+            expect(dispatchSpy).to.have.been.calledWith({
+                type: 'INPUT_CHANGE',
+                formKey: 'testForm',
+                fieldName: 'uuid',
+                entityPath: 'user',
+                value: 'new value'
+            });
+        });
+        it('should update the state', () => {
+            const uuidField = find(capturedProps.fields, field => field.name === 'uuid');
+            expect(uuidField.inputValue).to.equal('new value');
+        });
+        it('should reflect the change in the getUserInput method', () => {
+            expect(capturedProps.getUserInput().uuid).to.equal('new value');
+        });
+    })
+})
