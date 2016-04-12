fd28e4dc2a759be80c8a80de2f26878b277c83f2
Pierre Besson <pierre.besson7@gmail.com>
2016-04-12 06:47:44 +0200

    [master data] Update behaviour structure.

--- src/behaviours/master-data.js
+++ src/behaviours/master-data.js
@@ -1,21 +1,48 @@
 import React, {Component, PropTypes} from 'react';
 import {loadMasterData} from '../actions/master-data';
+import {pick} from 'lodash/object';
+
+const MASTER_DATA_PROPTYPE = PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      service: PropTypes.func.isRequired,
+      cacheDuration: PropTypes.number
+})).isRequired;
 
 const MASTER_DATA_CONTEXT_TYPE = {
-  masterDataloaders: PropTypes.object
+  masterDataloaders: MASTER_DATA_PROPTYPE
 }
 
-class MasterDataProvider extends Component {
-  constructor(props) {
-    super(props);
-    this._loaders = props.configuration.reduce((res, current) => {
-        res[current.name] = () => loadMasterData(name, current.service, current.cacheDuration);
-        return res;
-    });
+
+// Connect the component to the load method of the master data.
+const connectMasterData = (names) => {
+  return function connectComponent(ComponentToConnect) {
+      function MasterDataConnectedComponent(props, {masterDataloaders}) {
+        console.log(masterDataloaders);
+        const wantedMasterDataLoaders = masterDataloaders.reduce((res, current) => {
+              if(names.indexOf(current.name) !== -1){
+                res[`loadMasterData${current.name}`] = () => loadMasterData(current.name, current.service, current.cacheDuration);
+              }
+              return res;
+          }, {});
+          const {_behaviours, ...otherProps} = props;
+          const behaviours = {isConnectedMasterData: true, ..._behaviours}
+          console.log('wantedMasterDataLoaders', masterDataloaders, wantedMasterDataLoaders);
+          return <ComponentToConnect {...otherProps} {...wantedMasterDataLoaders} _behaviours={behaviours} />;
+      }
+      MasterDataConnectedComponent.displayName = `${ComponentToConnect.displayName}MasterDataConnectedComponent`;
+      MasterDataConnectedComponent.contextTypes = MASTER_DATA_CONTEXT_TYPE;
+      return MasterDataConnectedComponent;
   }
+
+};
+export const connect = connectMasterData;
+
+
+class MasterDataProvider extends Component {
   getChildContext() {
       return {
-        masterDataloaders: this._loaders
+        masterDataloaders: this.props.configuration
     }
   }
     render() {
@@ -27,13 +54,7 @@ MasterDataProvider.childContextTypes = MASTER_DATA_CONTEXT_TYPE;
 MasterDataProvider.defaultProps = {configuration: []}
 
 MasterDataProvider.propTypes = {
-  configuration:  PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        service: PropTypes.func.isRequired,
-        cacheDuration: PropTypes.number
-    })
-  ).isRequired;
+  configuration: MASTER_DATA_PROPTYPE
 };
 
-export const Provider = DefinitionsProvider;
+export const Provider = MasterDataProvider;
