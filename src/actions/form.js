9cf2e5b0cbc11d8151b58406e091d1cf7a630090
bernardstanislas <bernardstanislas@gmail.com>
2016-04-14 14:12:07 +0200

    Fix missing constants

--- src/actions/form.js
+++ src/actions/form.js
@@ -1,10 +1,12 @@
 export const CREATE_FORM = 'CREATE_FORM';
 export const DESTROY_FORM ='DESTROY_FORM';
 
+export const SYNC_FORM_ENTITY = 'SYNC_FORM_ENTITY';
+
 export const INPUT_CHANGE = 'INPUT_CHANGE';
 export const INPUT_ERROR = 'INPUT_ERROR';
 
-export const SYNC_FORM_ENTITY = 'SYNC_FORM_ENTITY';
+export const TOGGLE_FORM_EDITING = 'TOGGLE_FORM_EDITING';
 
 /**
  * Form creation action
