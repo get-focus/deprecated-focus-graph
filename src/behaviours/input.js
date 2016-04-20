e5819157708b5e0bb8c7b01c7785e7b281a85798
bernardstanislas <bernardstanislas@gmail.com>
2016-04-20 11:50:08 +0200

    Add inputBehaviour

---
+++ src/behaviours/input.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+const InputBehaviour = InputComponent => class WrappedInputComponent extends Component {
+    state = {typing: false};
+
+    onTypingBeginning() {
+        this.setState({typing: true});
+    }
+
+    onTypingEnd() {
+        this.setState({typing: false});
+    }
+
+    render() {
+        const {onTypingBeginning, onTypingEnd} = this;
+        const {typing} = this.state;
+        return (<InputComponent {...this.props} onTypingBeginning={onTypingBeginning} onTypingEnd={onTypingEnd} typing={typing}/>)
+    }
+};
+
+export default InputBehaviour;
