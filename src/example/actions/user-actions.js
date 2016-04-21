71afb7e57a62d8c235af30e0467dbfe1b27034f2
Pierre Besson <pierre.besson7@gmail.com>
2016-04-21 08:52:54 +0200

    [example] Create an action with address and user loaded.

--- src/example/actions/user-actions.js
+++ src/example/actions/user-actions.js
@@ -1,9 +1,14 @@
 import {actionBuilder} from '../../actions/entity-actions-builder';
-import {loadUserSvc, saveUserSvc} from '../services/user-service';
+import {loadUserSvc, saveUserSvc, loadMixedEntities} from '../services/user-service';
 
 const loadAction = actionBuilder({names: ['user'], type: 'load', service: loadUserSvc});
 export const loadUserTypes = loadAction.types;
 export const loadUserAction = loadAction.action;
+
+const _loadMixedAction = actionBuilder({names: ['user', 'address'], type: 'load', service: loadMixedEntities});
+export const loadMixedTypes = _loadMixedAction.types;
+export const loadMixedAction = _loadMixedAction.action;
+
 
 const saveAction = actionBuilder({names: ['user'], type: 'save', service: saveUserSvc});
 export const saveUserTypes = saveAction.types;
