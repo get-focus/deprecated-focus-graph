349b964b8446a11a70491675e23f24db48e93aa0
Pierre Besson <pierre.besson7@gmail.com>
2016-05-31 12:41:07 +0200

    [config] Extract config: domains, definitions, and master data from the index.

---
+++ src/example/config.js
@@ -0,0 +1,53 @@
+import moment from 'moment';
+import {loadCivility} from './services/load-civility';
+const format = ['DD/MM/YYYY', 'DD-MM-YYYY', 'D MMM YYYY'];
+
+export const definitions = {
+    user: {
+        uuid: { domain: 'DO_RODRIGO', isRequired: false},
+        firstName: { domain: 'DO_RODRIGO', isRequired: false},
+        lastName: { domain: 'DO_DON_DIEGO', isRequired: true},
+        date: { domain: 'DO_DATE', isRequired: false},
+        civility: { domain: 'DO_CIVILITE', isRequired: true}
+    },
+    address: {
+        uuid: { domain: 'DO_RODRIGO', isRequired: false},
+        city: { domain: 'DO_DON_DIEGO', isRequired: true}
+    }
+}
+
+export const domains = {
+    DO_RODRIGO: {
+        type: 'text',
+        validator: [{
+            type: 'string',
+            options: {
+                maxLength: 50
+            }
+        }]
+    },
+    DO_DON_DIEGO: {
+        type: 'text',
+        validator: [{
+            type: 'string',
+            options: {
+                maxLength: 200
+            }
+        }],
+        formatter: value => value + ' - formaté'
+    },
+    DO_DATE : {
+        formatter: date => date ? moment(date, format).format('DD/MM/YYYY') : ''
+    },
+    DO_CIVILITE: {
+        type: 'text',
+        validator: [{
+            type: 'string',
+            options: {
+                maxLength: 200
+            }
+        }]
+    }
+};
+
+export const masterDataConfig  = [{name: 'civility', service: loadCivility}];
