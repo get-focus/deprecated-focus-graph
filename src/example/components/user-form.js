90ef79c1e9361032fe47a9f3429fa1adc3934faa
Amélie <viollet.ame@gmail.com>
2016-09-07 12:10:43 +0200

    Select fields by form key (#87)
    * fix using select with no reference list
    
    * Selector fields by formKey to use the fieldHelper without the form

--- src/example/components/user-form.js
+++ src/example/components/user-form.js
@@ -1,9 +1,11 @@
 import React, {Component, PropTypes} from 'react';
+import {connect as connectToState} from 'react-redux';
 import {connect as connectToForm } from '../../behaviours/form';
 import {connect as connectToMetadata} from '../../behaviours/metadata';
 import {connect as connectToFieldHelpers} from '../../behaviours/field';
 import {connect as connectToMasterData} from '../../behaviours/master-data';
 import {loadUserAction, saveUserAction} from '../actions/user-actions';
+import {selectFieldsByFormKey} from '../../store/create-store';
 
 import LineComponent from '../../components/line'
 
@@ -13,18 +15,43 @@ import compose from 'lodash/flowRight';
 class UserForm extends Component {
     componentWillMount() {
         const {id, load} = this.props;
-        load({id});
     }
 
     render() {
         const {editing, fields, fieldFor,listFor,selectFor, loading, saving, list} = this.props;
-        console.log(fields)
+        return (
+            <Panel title='User' {...this.props}>
+              {fieldFor('firstName', {entityPath: 'user.information'})}
+            </Panel>
+        );
+    }
+};
+
+UserForm.displayName = 'UserForm';
+
+
+const ConnectedUserForm = compose(
+    connectToState(selectFieldsByFormKey('userForm')),
+    connectToMetadata(['user']),
+    connectToFieldHelpers()
+)(UserForm);
+
+
+class UserFormConfig extends Component {
+    componentWillMount() {
+        const {id, load} = this.props;
+        load({id});
+    }
+
+    render() {
+        const {editing, fields, fieldFor,listFor, loading, saving, list} = this.props;
         return (
             <Panel title='User' {...this.props}>
                 {fieldFor('uuid', {entityPath: 'user.information', onChange: () => {console.log(fields)}})}
                 {fieldFor('firstName', {entityPath: 'user.information'})}
                 {fieldFor('lastName', {entityPath: 'user.information'})}
                 {fieldFor('date', {entityPath: 'user.information'})}
+                {fieldFor('test', {entityPath: 'user.information'})}
                 {listFor('childs', {LineComponent, entityPath : 'user.information', redirectEntityPath: 'user.child'})}
 
             </Panel>
@@ -32,7 +59,7 @@ class UserForm extends Component {
     }
 };
 
-UserForm.displayName = 'UserForm';
+UserFormConfig.displayName = 'UserFormConfig';
 
 const formConfig = {
     formKey: 'userForm',
@@ -42,11 +69,19 @@ const formConfig = {
     nonValidatedFields: ['user.information.firstName']
 };
 
-//Connect the component to all its behaviours (respect the order for store, store -> props, helper)
-const ConnectedUserForm = compose(
+const ConnectedUserFormConfig = compose(
     connectToMetadata(['user']),
     connectToForm(formConfig),
     connectToFieldHelpers()
-)(UserForm);
+)(UserFormConfig);
 
-export default ConnectedUserForm;
+
+function ComponentUser(props)  {
+  return <div>
+    <ConnectedUserForm {...props}/>
+    <ConnectedUserFormConfig {...props}/>
+  </div>
+
+}
+
+export default ComponentUser;
