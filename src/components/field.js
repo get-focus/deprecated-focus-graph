f428a383bf5237d0b797248ef916e7fc26da7f89
Ephrame <viollet.ame@gmail.com>
2016-09-14 15:48:09 +0200

    [FormBehaviours] action for the dispatch

--- src/components/field.js
+++ src/components/field.js
@@ -20,6 +20,7 @@ class Field extends PureComponent {
   render(){
     const {textOnly, multiple, list, fieldForLine, ...otherProps} = this.props;
     otherProps.value = otherProps.rawInputValue; //https://github.com/get-focus/focus-redux/issues/39 compatibility with focus components
+    console.log(otherProps.value)
     const {TextComponent = DefaultTextComponent, DisplayComponent = DefaultDisplayComponent, InputComponent = DefaultInputComponent, SelectComponent = DefaultSelectComponent,SelectComponentDisplay = DefaultSelectDisplayComponent, ListComponent = DefaultListComponent} = otherProps.metadata;
     const renderConsult = () => list ?  <ListComponent fieldForLine={fieldForLine} values={otherProps.formattedInputValue} {...otherProps}/> : (multiple ? <SelectComponentDisplay {...otherProps} /> : <DisplayComponent {...otherProps} />);
     const renderEdit = () => list ? <ListComponent fieldForLine={fieldForLine} values={otherProps.formattedInputValue} {...otherProps}/> : (multiple ? <SelectComponent {...otherProps}/> : <InputComponent {...otherProps}/>);
