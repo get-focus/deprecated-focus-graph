4208bab6abc0316328a323cbf573c7a23c267f5b
Ephrame <viollet.ame@gmail.com>
2016-09-16 11:02:34 +0200

    [fiedl] scss

--- src/components/field.js
+++ src/components/field.js
@@ -8,9 +8,9 @@ import DefaultTextComponent from './text';
 import DefaultSelectDisplayComponent from './select-display';
 
 const FieldLabelValueComponent = ({label, ValueComponent, editing}) => (
-    <div data-focus='field' data-mode={editing ? 'edit' : 'consult'}>
-        <div data-focus='label'><b>{label}</b></div>
-        <div data-focus='field-value-container'>{ValueComponent}</div>
+    <div data-focus='field' className='mdl-grid' data-mode={editing ? 'edit' : 'consult'}>
+        <div data-focus='field-label-container' className='mdl-cell mdl-cell--top mdl-cell--4-col'><b><div data-focus='label'>{label}</div></b></div>
+        <div data-focus='field-value-container' className='mdl-cell mdl-cell--top mdl-cell--8-col'>{ValueComponent}</div>
     </div>
 );
 FieldLabelValueComponent.displayName = 'FieldLabelValueComponent';
@@ -22,7 +22,7 @@ class Field extends PureComponent {
     const {textOnly, multiple, list, fieldForLine, ...otherProps} = this.props;
     otherProps.value = otherProps.rawInputValue; //https://github.com/get-focus/focus-redux/issues/39 compatibility with focus components
     const {TextComponent = DefaultTextComponent, DisplayComponent = DefaultDisplayComponent, InputComponent = DefaultInputComponent, SelectComponent = DefaultSelectComponent,SelectComponentDisplay = DefaultSelectDisplayComponent, ListComponent = DefaultListComponent} = otherProps.metadata;
-    const renderConsult = () => list ?  <ListComponent fieldForLine={fieldForLine} values={otherProps.formattedInputValue} {...otherProps}/> : (multiple ? <SelectComponentDisplay {...otherProps} /> : <InputComponent {...otherProps} />);
+    const renderConsult = () => list ?  <ListComponent fieldForLine={fieldForLine} values={otherProps.formattedInputValue} {...otherProps}/> : (multiple ? <SelectComponentDisplay {...otherProps} /> : <DisplayComponent  {...otherProps} />);
     const renderEdit = () => list ? <ListComponent fieldForLine={fieldForLine} values={otherProps.formattedInputValue} {...otherProps}/> : (multiple ? <SmartSelectComponent SelectComponent={SelectComponent} {...otherProps}/> : <InputComponent {...otherProps}/>);
     const ValueComponent = otherProps.editing ? renderEdit() : renderConsult();
     return textOnly ? ValueComponent : <FieldLabelValueComponent label={otherProps.name}  editing={otherProps.editing} ValueComponent={ValueComponent} />;
