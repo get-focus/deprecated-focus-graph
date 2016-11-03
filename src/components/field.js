c3f0e14556f45d6dbd64d2031c1d4cdf91d2a470
GuenoleK <kikabouguenole@gmail.com>
2016-11-03 18:01:54 +0100

    Correct merge on field component

--- src/components/field.js
+++ src/components/field.js
@@ -43,10 +43,6 @@ Field.displayName = 'Field';
 Field.propTypes = {
     error: PropTypes.string,
     name: PropTypes.string.isRequired,
-    multiple: PropTypes.bool,
-    hasLabel: PropTypes.bool
-};
-Field.defaultProps = {
-    hasLabel: true
+    multiple: PropTypes.bool
 };
 export default Field;
