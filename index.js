4c457e0026bc355200af05de7ce699e056b5ecbe
Thomas <tgallon@gmail.com>
2016-12-01 15:51:59 +0100

    fix ignore

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function () {
-    console.log("Focus redux.... so great....");
+    console.log("Tools based on Redux to easily integrate forms, validate and manipulate data. http://getfocus.io/focus-documentation/");
 };
 
 module.exports = exports['default'];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7OztrQkFBZSxZQUFNO0FBQ2pCQSxZQUFRQyxHQUFSO0FBQ0gsQyIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0ICgpID0+IHtcclxuICAgIGNvbnNvbGUubG9nKGBGb2N1cyByZWR1eC4uLi4gc28gZ3JlYXQuLi4uYClcclxufVxyXG4iXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7OztrQkFBZSxZQUFNO0FBQ2pCQSxZQUFRQyxHQUFSO0FBQ0gsQyIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0ICgpID0+IHtcclxuICAgIGNvbnNvbGUubG9nKGBUb29scyBiYXNlZCBvbiBSZWR1eCB0byBlYXNpbHkgaW50ZWdyYXRlIGZvcm1zLCB2YWxpZGF0ZSBhbmQgbWFuaXB1bGF0ZSBkYXRhLiBodHRwOi8vZ2V0Zm9jdXMuaW8vZm9jdXMtZG9jdW1lbnRhdGlvbi9gKTtcclxufVxyXG4iXX0=
