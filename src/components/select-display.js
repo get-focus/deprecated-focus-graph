79b1c0e02bce16a591c48ae3223bd0b97506ec2b
Ephrame <viollet.ame@gmail.com>
2017-02-06 16:51:24 +0100

    [SmartSelect] defaultValue

--- src/components/select-display.js
+++ src/components/select-display.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import reactReduxStoreShape from 'react-redux/lib/utils/storeShape';
 import find from 'lodash/find';
-
-
+import get from 'lodash/get'
+import i18n from 'i18next';
 function renderLabelOfCode(values, code){
   const element = values ? values.find(element => element.code === code) : [];
   const label = element ? element.label : "";
@@ -22,11 +22,14 @@ const SelectComponent = ({
 }, {store: {getState}}) => {
     const {masterData = []} = getState();
     const masterDatumObject = find(masterData, {name: masterDatum}) || {value: []};
+
     const {value: values} = masterDatumObject;
-    const label = renderLabelOfCode(values, rawInputValue);
+    const defaultValue = values ? values.find(element => element.isDefaultValue) : rawInputValue;
+
+    const label = renderLabelOfCode(values, rawInputValue || get(defaultValue, 'code'));
     return (
         <div>
-          {label}
+          {i18n.t(label)}
         </div>
     );
 };
--- src/components/smart-select.js
+++ src/components/smart-select.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import reactReduxStoreShape from 'react-redux/lib/utils/storeShape';
 import find from 'lodash/find';
+import compose from 'lodash/flowRight';
+import {connect as connectToState} from 'react-redux';
+
+import get from 'lodash/get';
 import isUndefined from 'lodash/isUndefined';
+import {selectReferenceList} from '../store/create-store';
+class SmartSelectComponent extends Component {
+    componentWillReceiveProps(newProps){
+        const selectValues = newProps.referenceList[0].value || [];
+        const defaultValue = selectValues.find(element => element.isDefaultValue);
+        if(isUndefined(this.props.rawInputValue) && defaultValue){
+            this.props.onChange(defaultValue.code)
+        }
+    }
+    render(){
+        const {
+            name,
+            rawInputValue,
+            formattedInputValue,
+            onChange,
+            error,
+            valid,
+            values = [],
+            referenceList =[],
+            masterDatum,
+            SelectComponent,
+            ...otherProps
+        } = this.props;
+
+        const selectValues = referenceList[0].value || [];
+        const defaultValue = selectValues.find(element => element.isDefaultValue);
+
+        const currentValue = isUndefined(rawInputValue) ? get(defaultValue, 'code') : rawInputValue;
+        return (
+            <SelectComponent onChange={onChange} error={error} formattedInputValue={formattedInputValue} name={name} values={selectValues} rawInputValue={currentValue} currentValue={currentValue}  valid={valid} defaultValue={defaultValue} {...otherProps}/>
+        )
+    }
 
 
-const SmartSelectComponent = ({
-    name,
-    rawInputValue,
-    formattedInputValue,
-    onChange,
-    error,
-    valid,
-    values,
-    masterDatum,
-    SelectComponent,
-    ...otherProps
-}, {store: {getState}}) => {
-    const {masterData = []} = getState();
-    const masterDatumObject = find(masterData, {name: masterDatum}) || {value: []};
-    const {value} = masterDatumObject;
-    const selectValues = values || value
-    const defaultValue = selectValues.find(element => element.isDefaultValue);
-    const currentValue = isUndefined(rawInputValue) ? defaultValue : rawInputValue;
-    return <SelectComponent onChange={onChange} error={error} formattedInputValue={formattedInputValue} name={name} values={selectValues} rawInputValue={rawInputValue} valid={valid} defaultValue={defaultValue} {...otherProps}/>
-};
-
-SmartSelectComponent.contextTypes = {
-    store: reactReduxStoreShape
-};
-
-export default SmartSelectComponent;
+}
+export default  compose(
+    connectToState(selectReferenceList())
+)(SmartSelectComponent)
